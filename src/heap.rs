4c6ace3013c10e96c4b213301c122e05e4887c44
Linwei Zhang <lewiszlw520@gmail.com>
2024-12-06 14:34:08 +0800

    Improve ld script

--- src/heap.rs
+++ src/heap.rs
@@ -8,13 +8,13 @@ static HEAP_ALLOCATOR: LockedHeap<32> = LockedHeap::new();
 
 pub fn init_heap() {
     extern "C" {
-        fn image_end();
+        fn free_mem_start();
     }
     unsafe {
         // 1G
         let heap_size = 1024 * 1024 * 1024;
-        HEAP_ALLOCATOR.lock().init(image_end as usize, heap_size);
-        println!("inited heap: [{:#x}, {:#x})", image_end as usize, image_end as usize + heap_size);
+        HEAP_ALLOCATOR.lock().init(free_mem_start as usize, heap_size);
+        println!("inited heap: [{:#x}, {:#x})", free_mem_start as usize, free_mem_start as usize + heap_size);
     }
 
     heap_test();
